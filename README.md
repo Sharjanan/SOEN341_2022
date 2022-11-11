218e1a3d2026879b1d24d28b30bc5726e9ffa5ad
Sharjanan <98242877+Sharjanan@users.noreply.github.com>
2022-11-10 22:54:36 -0500

    updated files

---
+++ README.md
@@ -0,0 +1,28 @@
+# SOEN341_2022
+
+## Objective
+
+-----
+
+## Project Description
+
+  ---
+## Core Features 
+---
+
+
+## Team members
+| Name | GitHub user name |
+| ------------- | ------------- |
+| Sharjanan Staniculas  | sharjanan  |
+| Mohammad Usman | 0-ANONU-0  |
+|-| - |
+| - | -|
+| -| - |
+| -|- |
+| -| - |
+
+## Languages & Techniques
+
+
+
---
+++ login.php
@@ -0,0 +1,33 @@
+<?php include "header.php";?>
+<section class="login_page">
+
+<div class="login_box">
+    <form id="login_form">
+        <h3><b>LOGIN</b></h3>
+        <label for="email">Email:</label><br>
+        <input type="text" id="email" name="email"><br>
+        <label for="password">Password:</label><br>
+        <input type="text" id="password" name="password">
+        <input type="checkbox" id="remember" name="remember" value="Remember me? ">
+        <label for="remember"> Remember me?</label><br><br>
+        <!-- choice for user type -->
+        <input type="radio" id="user" name="user_type" value="USER">
+        <label for="user">User</label>
+        <input type="radio" id="admin" name="user_type" value="ADMIN">
+        <label for="admin">Admin</label>
+        <input type="radio" id="provider" name="user_type" value="PROVIDER">
+        <label for="provider">Provider</label><br><br>
+
+        <div class="center">
+            <input type="submit" name="submit" value="LOGIN" class="login_button">
+        </div>
+        <p>
+           Need an account? <a href = "#signup"> <u>SIGN UP</u></a>
+        </p>
+    </form><br>
+
+    <!-- <button type="submit" form="login_form" value="Submit" class="login_button">LOGIN</button> -->
+</div>
+
+</section>
+<?php include "footer.php";?>
