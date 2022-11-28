b691e3f898d929911e2bf744f4e4197df7f93e85
Niraj <114127591+nirajptl16@users.noreply.github.com>
2022-11-27 22:18:02 -0500

    Add files via upload

--- config.php
+++ config.php
@@ -1,19 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-//for later
-$db= "";
 
-
-// Create connection
-$link = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($link->connect_error) {
-  die("Connection failed: " . $link->connect_error);
-}
-echo "Connected successfully";
-
+$conn = mysqli_connect('localhost','root','','cart_db');
 
 ?>
