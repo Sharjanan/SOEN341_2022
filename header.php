6373ccf793a2ee7a6e25ccfa183ad2ae9da53caf
Niraj <114127591+nirajptl16@users.noreply.github.com>
2022-12-05 12:53:30 -0500

    Update header.php

--- header.php
+++ header.php
@@ -22,12 +22,8 @@ if (session_status() === PHP_SESSION_NONE) {
                 <a href="index.php" class="w3-bar-item w3-button w3-wide">LOGO</a>
                 <!-- Right-sided navbar links -->
                 <div class="w3-right w3-hide-small">
-
-                <a href="product.php" class="w3-bar-item w3-button">PRODUCT</a>
-                <a href="#team" class="w3-bar-item w3-button"><i class="fa fa-user"></i> TEAM</a>
-                <a href="#work" class="w3-bar-item w3-button"><i class="fa fa-th"></i> WORK</a>
-                <a href="#pricing" class="w3-bar-item w3-button"><i class="fa fa-usd"></i> PRICING</a>
-                <a href="#contact" class="w3-bar-item w3-button"><i class="fa fa-envelope"></i> CONTACT</a>
+                <a href="product.php" class="w3-bar-item w3-button">PRODUCTS</a>
+                <a href="#team" class="w3-bar-item w3-button"></i> SHOPPING CART</a>
                 <a href="login.php" class="w3-bar-item w3-button">Sign in</a>
                 </div>
                 <!-- Hide right-floated links on small screens and replace them with a menu icon -->
@@ -42,13 +38,8 @@ if (session_status() === PHP_SESSION_NONE) {
         <nav class="w3-sidebar w3-bar-block w3-black w3-card w3-animate-left w3-hide-medium w3-hide-large" style="display:none" id="mySidebar">
         <a href="javascript:void(0)" onclick="w3_close()" class="w3-bar-item w3-button w3-large w3-padding-16">Close ×</a>
         <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button">PRODUCT</a>
-        <a href="#team" onclick="w3_close()" class="w3-bar-item w3-button">TEAM</a>
-        <a href="#work" onclick="w3_close()" class="w3-bar-item w3-button">WORK</a>
-        <a href="#pricing" onclick="w3_close()" class="w3-bar-item w3-button">PRICING</a>
-        <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button">CONTACT</a>
         <a href="login.html" onclick="w3_close()" class="w3-bar-item w3-button">Sign in</a>
         </nav>
     </body>
 
 <!-- </html in footer -->
-
