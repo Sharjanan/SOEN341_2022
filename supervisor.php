4dde555ba3bc881091e47bd9443cc02edc1bea51
MfurayaseD <dkmfurayase@gmail.com>
2022-12-05 20:40:54 -0500

    Create supervisor.php
    Frontend Supervisor page

---
+++ supervisor.php
@@ -0,0 +1,52 @@
+<?php include "header.php"; ?>
+<link rel="stylesheet" href="./css/supervisor.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<body>
+ <div class="Cart-Container">
+ <div class="Header">
+ <h3 class="Heading">Customer Products</h3>
+ <h5 class="Action"><i class="fa fa-shopping-cart"></i> Prices</h5>
+ </div>
+ <div class="Cart-Items">
+ <div class="image-box">
+<img src="./products/Airpods2.jpg" height="120"><p>PhoneType Airpods</a>
+ </div>
+ <div class="about">
+ <h1 class="title">Wireless Bluetooth airpods</h1>
+ </div>
+ <div class="counter"></div>
+ <div class="prices"></div>
+ <div class="prices">
+ <div class="amount">$179.00</div>
+ </div>
+ </div>
+ 
+ <!-- cart item 2 -->
+ <div class="Cart-Items">
+ <div class="image-box">
+ <img src="./products/TV1.jpg" height="120"><p>Television Set</a>
+ </div>
+ <div class="about">
+ <h1 class="title">Smart TV Samusang UHD</h1>
+ </div>
+ <div class="prices">
+ <div class="amount">$4999.99</div>
+ </div>
+ </div>
+ 
+ <hr> 
+ <div class="checkout">
+ <div class="total">
+ <div>
+ <div class="Subtotal">Sub-Total</div>
+ <div class="items">2 items</div>
+ </div>
+ <div class="total-amount">$5178.99</div>
+ </div>
+ <button class="button">Approve Purchase</button>
+ <button class="button2">Reject Quotation</button>
+ </div>
+ 
+ </div>
+</body>
+
