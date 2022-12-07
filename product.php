ef2f92e570a891d2c7e976a9befacc108a717fe7
Sharjanan <98242877+Sharjanan@users.noreply.github.com>
2022-12-06 19:56:28 -0500

    items tooken from db

--- product.php
+++ product.php
@@ -1,95 +1,42 @@
 <?php include "header.php"; ?>
 
+<?php
+require_once "config.php";
+
+$sql = "SELECT * FROM products";
+$result = $link->query($sql);
+
+ 
+?> 
 <link rel="stylesheet" href="./css/product.css">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <section class="product_page">
 <h1> List of Products </h1>
+
+<!-- PHP CODE TO FETCH DATA FROM ROWS -->
+            <?php
+                // LOOP TILL END OF DATA
+                while($rows=$result->fetch_assoc())
+                {
+            ?>
   <!--product1 -->
+   <!-- FETCHING DATA FROM EACH
+                    ROW OF EVERY COLUMN -->
   <div class="row">
     <div class="column">
       <div class="card">
-        <img src="./products/Airpods1.jpg" alt="Denim Jeans" style="width:100%">
-        <h1>Tailored Jeans</h1>
-        <p class="price">$19.99</p>
-        <p>Some text about the perfect jeans..</p>
+        <img src="./products/<?php echo $rows['product_img'];?>" alt="Denim Jeans" style="width:100%">
+        <h3><?php echo $rows['product_name'];?></h3>
+        <p class="price"><?php echo $rows['product_price'];?></p>
+       
         <p><button>Add to Cart</button></p>
       </div>
     </div>
   </div>
-  <!--product2 -->
-  <div class="row">
-    <div class="column">
-      <div class="card">
-        <img src="./products/Airpods2.jpg" alt="Denim Jeans" style="width:100%">
-        <h1>Tailored Jeans</h1>
-        <p class="price">$19.99</p>
-        <p>Some text about the perfect jeans..</p>
-        <p><button>Add to Cart</button></p>
-      </div>
-    </div>
-  </div>
-   <!--product3 -->
-  <div class="row">
-    <div class="column">
-      <div class="card">
-        <img src="./products/Airpods3.jpg" alt="Denim Jeans" style="width:100%">
-        <h1>Tailored Jeans</h1>
-        <p class="price">$19.99</p>
-        <p>Some text about the perfect jeans..</p>
-        <p><button>Add to Cart</button></p>
-      </div>
-    </div>
-  </div>
-     <!--product3 -->
-     <div class="row">
-    <div class="column">
-      <div class="card">
-        <img src="./products/SportBag2.jpg" alt="Denim Jeans" style="width:100%">
-        <h1>Tailored Jeans</h1>
-        <p class="price">$19.99</p>
-        <p>Some text about the perfect jeans..</p>
-        <p><button>Add to Cart</button></p>
-      </div>
-    </div>
-  </div>
-     <!--product3 -->
-     <div class="row">
-    <div class="column">
-      <div class="card">
-        <img src="./products/TV3.jpg" alt="Denim Jeans" style="width:100%">
-        <h1>Tailored Jeans</h1>
-        <p class="price">$19.99</p>
-        <p>Some text about the perfect jeans..</p>
-        <p><button>Add to Cart</button></p>
-      </div>
-    </div>
-  </div>
-  </div>
-     <!--product3 -->
-     <div class="row">
-    <div class="column">
-      <div class="card">
-        <img src="./products/TV2.jpg" alt="Denim Jeans" style="width:100%">
-        <h1>Tailored Jeans</h1>
-        <p class="price">$19.99</p>
-        <p>Some text about the perfect jeans..</p>
-        <p><button>Add to Cart</button></p>
-      </div>
-    </div>
-  </div>
-  </div>
-     <!--product3 -->
-     <div class="row">
-    <div class="column">
-      <div class="card">
-        <img src="./products/TV1.jpg" alt="Denim Jeans" style="width:100%">
-        <h1>Tailored Jeans</h1>
-        <p class="price">$19.99</p>
-        <p>Some text about the perfect jeans..</p>
-        <p><button>Add to Cart</button></p>
-      </div>
-    </div>
-  </div>
+            <?php
+                }
+            ?>
+
 </section>
 
 <?php include "footer.php"; ?>
